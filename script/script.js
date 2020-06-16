c54cf1844f4f56ada6e43906afb4ddbe3de99211
Ziphil <Ziphil@users.noreply.github.com>
2020-06-16 10:01:33 +0000

    Deploy Ziphil/ShaleianSlide@28b7d7a25f6fd77fb247bf1000911794100272ba

--- script/script.js
+++ script/script.js
@@ -6,12 +6,12 @@ let index = 0;
 function prepare() {
   let size = document.querySelectorAll("*[class$='slide']").length;
   document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowLeft") {
+    if (event.key === "ArrowLeft" || event.key === "ArrowUp" || event.key === "Backspace") {
       if (index > 0) {
         index --;
       }
       scroll();
-    } else if (event.key === "ArrowRight") {
+    } else if (event.key === "ArrowRight" || event.key === "ArrowDown" || event.key === "Enter" || event.key === " ") {
       if (index < size - 1) {
         index ++;
       }
